0050778a88213062842259ce6bf2b4aad149e9dd
youpeng.zhang <youpeng.zhang@chinaredstar.com>
2018-05-16 21:39:34 +0800

    Lambda 表达式test

--- src/main/java/java8Test/Example1.java
+++ src/main/java/java8Test/Example1.java
@@ -2,6 +2,7 @@ package java8Test;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -65,13 +66,44 @@ public class Example1 {
 //        System.out.printf("Original List : %s, filtered list : %s %n", stringList, filterNew);
 
 
-        List<String> G7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.","Canada");
-        String G7Countries = G7.stream().map(x -> x.toUpperCase()).collect(Collectors.joining(", "));
-        System.out.println(G7Countries);
+//        List<String> G7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.","Canada");
+//        String G7Countries = G7.stream().map(x -> x.toUpperCase()).collect(Collectors.joining(", "));
+//        System.out.println(G7Countries);
 
-        List<String> g8counties = G7.stream().map(x->x.toUpperCase()).collect(Collectors.toList());
-        System.out.println(g8counties);
+        // 用所有不同的数字创建一个正方形列表
+//        List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
+//        List<Integer> distinct = numbers.stream().map( i -> i*i).distinct().collect(Collectors.toList());
+////        List<Integer> distinct = numbers.stream().map( i -> i*i).collect(Collectors.toList());
+//        System.out.printf("Original List : %s,  Square Without duplicates : %s %n", numbers, distinct);
 
+        //获取数字的个数、最小值、最大值、总和以及平均值
+//        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+//        IntSummaryStatistics stats = primes.stream().mapToInt((x) -> x).summaryStatistics();
+//
+//        System.out.println("Highest prime number in List : " + stats.getMax());
+//        System.out.println("Lowest prime number in List : " + stats.getMin());
+//        System.out.println("Sum of all prime numbers : " + stats.getSum());
+//        System.out.println("Average of all prime numbers : " + stats.getAverage());
+
+
+//        List<Integer> primes = Arrays.asList(new Integer[]{2, 3,5,7});
+//        int factor = 2;
+//        primes.forEach(p -> System.out.println(p*factor));
+//        primes.forEach(element -> { System.out.println(factor*element); });
+//        primes.forEach(p -> {System.out.println(p*factor);});
+
+//        String[] atp = {"Rafael Nadal", "Novak Djokovic",
+//                "Stanislas Wawrinka",
+//                "David Ferrer","Roger Federer",
+//                "Andy Murray","Tomas Berdych",
+//                "Juan Martin Del Potro"};
+//        List<String> players =  Arrays.asList(atp);
+//        players.forEach((play) -> System.out.print(play + ";"));
+//        players.forEach(System.out::println);
+
+        new Thread(() -> System.out.println("hello Jeff!")).start();
+        Runnable runnable = () -> System.out.println("hello jeff2 !");
+        new Thread(runnable).start();
     }
 
 
