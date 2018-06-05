3145b01ba2a922351a05918a7b4becfe5358661a
youpengzhang@163.com <youpengzhang@163.com>
2018-06-05 13:34:18 +0800

    修改文件

--- src/main/java/testExample/Varable.java
+++ src/main/java/testExample/Varable.java
@@ -1,12 +1,25 @@
 package testExample;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * @author Youpeng.Zhang on 2018/6/5.
  */
 public class Varable {
+    public static String readFirstLineFromFile(String path) throws IOException {
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            return br.readLine();
+        }
+    }
+
     public static void main(String [] args){
         System.out.println(add(2,3));
         System.out.println(add(2,3,5));
+
+
     }
     public static int add(int x,int ...args){
         int sum=x;
