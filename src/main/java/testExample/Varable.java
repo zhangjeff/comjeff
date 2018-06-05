79b61f89f8beb0d4d2f297a13ff2b59c44c550c5
youpengzhang@163.com <youpengzhang@163.com>
2018-06-05 13:20:57 +0800

    修改文件

--- src/main/java/testExample/Varable.java
+++ src/main/java/testExample/Varable.java
@@ -13,6 +13,11 @@ public class Varable {
         for(int i=0;i<args.length;i++){
             sum+=args[i];
         }
+
+        int num = 1234_5678_9;
+        String aa = Integer.toString(num);
+        System.out.println(num);
+        System.out.println(aa);
         return sum;
     }
 }
