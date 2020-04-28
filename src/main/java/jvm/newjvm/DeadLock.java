247a8a762419668481daf4c11606042fe41a67be
youpeng.zhang <youpeng.zhang@chinaredstar.com>
2020-04-28 11:12:39 +0800

    dead lock

---
+++ src/main/java/jvm/newjvm/DeadLock.java
@@ -0,0 +1,36 @@
+package jvm.newjvm;
+
+public class DeadLock {
+
+    public static void main(String[] args) {
+       new Thread(A::getA,"ThreadA").start();
+
+       new Thread(B::getB,"ThreadB").start();
+    }
+}
+
+
+class A {
+
+    public static synchronized void getA(){
+        System.out.println("进入A");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        B.getB();
+    }
+}
+
+class B {
+    public static synchronized void getB(){
+        System.out.println("进入B");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        A.getA();
+    }
+}
