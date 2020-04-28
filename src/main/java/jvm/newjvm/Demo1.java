0eb435ce77f77722f93d2e91513b0ee46e309e09
youpeng.zhang <youpeng.zhang@chinaredstar.com>
2020-04-28 14:01:33 +0800

    dead lock

---
+++ src/main/java/jvm/newjvm/Demo1.java
@@ -0,0 +1,35 @@
+package jvm.newjvm;
+
+public class Demo1 {
+
+//    private static  Iinteger a;
+//    Integer b;
+    public static void main(String[] args) {
+        int i = 2;
+        i = i++;
+        System.out.println(i);
+
+
+//        System.out.println(a);
+//        Demo1 demo1 = new Demo1();
+//        System.out.println(demo1.b);
+//        System.out.println(StaticTest.aa);
+//        System.out.println(StaticTest2.bb);
+//
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//            System.out.println("33333333");
+//        }
+    }
+}
+
+//  -XX:+TraceClassLoading
+//class StaticTest{
+//    public final  static String aa = "aaaaaaaaaaaa";
+//}
+//
+//class StaticTest2{
+//    public static String bb = "ccccccccccccccc";
+//}
