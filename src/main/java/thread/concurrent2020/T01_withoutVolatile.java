0d5763de35eb624dfeb98f4cb2d189f4521f41b5
youpeng.zhang <youpeng.zhang@chinaredstar.com>
2020-05-19 11:40:08 +0800

    commit

---
+++ src/main/java/thread/concurrent2020/T01_withoutVolatile.java
@@ -0,0 +1,49 @@
+package thread.concurrent2020;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class T01_withoutVolatile {
+
+    volatile List list = new LinkedList();
+//    volatile  List list = Collections.synchronizedList(new LinkedList());
+
+    public  void add(Object o) {
+        list.add(o);
+    }
+
+    public  int size(){
+        return list.size();
+    }
+
+    public static void main(String[] args) {
+        T01_withoutVolatile t01_withoutVolatile = new T01_withoutVolatile();
+        new Thread(()->{
+            for (int i = 0; i < 10; i++) {
+                Object o = new Object();
+                t01_withoutVolatile.add(o);
+                System.out.println("add" + i);
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }, "t1").start();
+
+        new Thread(()->{
+            while(true) {
+                if (t01_withoutVolatile.size() == 5){
+                    break;
+                }
+            }
+            System.out.println("线程二结束");
+        },"t2").start();
+
+    }
+}
