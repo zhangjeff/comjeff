49f9d44c262359b4836aea7be465410f4fcb8342
youpeng.zhang <youpeng.zhang@chinaredstar.com>
2020-06-16 10:02:59 +0800

    commit

---
+++ src/main/java/jvm/newjvm/SingleInstance.java
@@ -0,0 +1,23 @@
+package jvm.newjvm;
+
+public class SingleInstance {
+
+    private volatile SingleInstance singleInstance;
+
+    private Object obj;
+
+    private SingleInstance(){
+    }
+
+    public SingleInstance getInstance(){
+        if (singleInstance == null) {
+            synchronized (obj) {
+                if (singleInstance == null) {
+                    singleInstance = new SingleInstance();
+                    return singleInstance;
+                }
+            }
+        }
+        return singleInstance;
+    }
+}
